3c4f3e3d1bfeadb4c12a33071e513fc327962a53
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-05-25 17:02:31 +0700

    adding: sentry issue trackers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap';
-import 'bootstrap/dist/js/bootstrap.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap";
+import "bootstrap/dist/js/bootstrap.js";
 
+Sentry.init({
+  dsn: "https://6ba349e08fe840f9ac33ccea8ef93004@o722722.ingest.sentry.io/5781750",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
