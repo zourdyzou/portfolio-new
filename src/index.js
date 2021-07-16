618869b365297d7057a5018746b5be9857e05bca
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-16 11:57:27 +0700

    make an animation for the bonus components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import App from "./App";
@@ -17,12 +17,7 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
